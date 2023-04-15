3148ee5260351881d8a8ffe5210400ab8ed58928
Siwakorn Sairung <siwakorn_pipe@hotmail.com>
2023-04-15 17:01:54 +0700

    🎨 Improve video sampling script

--- sampling_video.py
+++ sampling_video.py
@@ -9,14 +9,15 @@ samples = 60
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-video_files = [f for f in os.listdir(video_dir) if os.path.isfile(os.path.join(video_dir, f)) and f.endswith(".mp4")]
+video_files = [f for f in os.listdir(video_dir) if os.path.isfile(os.path.join(video_dir, f)) and f.endswith(".mp4") or f.endswith(".MP4")]
 
 fps = 30
 frame_count = 1800
 frame_between_sample = int(frame_count/samples)
 
-for video_file in video_files:
+for video_number, video_file in enumerate(video_files):
 
+    print(f"\nProcessing video {video_number+1} of {len(video_files)}...")
     video_path = os.path.join(video_dir, video_file)
     video = cv2.VideoCapture(video_path)
 
@@ -24,6 +25,7 @@ for video_file in video_files:
     if not os.path.exists(frame_dir):
         os.makedirs(frame_dir)
 
+    file_number = 1
     for i in tqdm(range(0, frame_count, frame_between_sample)):
 
         video.set(cv2.CAP_PROP_POS_FRAMES, i)
@@ -32,9 +34,10 @@ for video_file in video_files:
         # Check if there was a problem reading the frame
         if not ret: break
         
-        frame_filename = os.path.join(frame_dir, f"frame{i}.jpg")
+        frame_filename = os.path.join(frame_dir, f"{file_number}_frame{i}.jpg")
         cv2.imwrite(frame_filename, frame)
+        file_number += 1
 
     video.release()
 
-    print(f"[{video_file}] Done sampling {samples} images to {output_dir}.")
+    print(f"Done sampling {samples} images from {video_file} to {frame_dir}")
